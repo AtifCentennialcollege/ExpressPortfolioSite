cd7f044a3aa3280e9b64b6e1f59418e151d913d3
AtifCentennialCollege <Athakor4@my.centennialcollege.ca>
2020-10-11 20:30:00 -0400

    Added to Projects page

--- routes/index.js
+++ routes/index.js
@@ -14,8 +14,8 @@ router.get('/aboutMe', function(req, res, next) {
   res.render('index', { title: 'About Me',});
 });
 
-router.get('/products', function(req, res, next) {
-  res.render('index', { title: 'Products',});
+router.get('/projects', function(req, res, next) {
+  res.render('index', { title: 'Projects',});
 });
 
 router.get('/services', function(req, res, next) {
