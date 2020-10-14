755cf3cbfdbdfaed2ba9da902c9d437358c2e89e
AtifCentennialCollege <Athakor4@my.centennialcollege.ca>
2020-10-13 23:01:30 -0400

    Added some comments

--- routes/index.js
+++ routes/index.js
@@ -5,23 +5,23 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home',});
 });
-
+/* Home */
 router.get('/home', function(req, res, next) {
   res.render('index', { title: 'Home',});
 });
-
+/* About Me */
 router.get('/aboutMe', function(req, res, next) {
   res.render('index', { title: 'About Me',});
 });
-
+/* Projects */
 router.get('/projects', function(req, res, next) {
   res.render('index', { title: 'Projects',});
 });
-
+/* Services */
 router.get('/services', function(req, res, next) {
   res.render('index', { title: 'Services',});
 });
-
+/* Contact Me */
 router.get('/contactMe', function(req, res, next) {
   res.render('index', { title: 'Contact Me',});
 });
